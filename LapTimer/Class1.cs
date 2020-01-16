c5b58db56eaf3edb837f95ed88841091252debad
David Liu <dvdgege5@gmail.com>
2020-01-16 11:26:03 -0800

    begin implementation of race mode

--- LapTimer/Class1.cs
+++ LapTimer/Class1.cs
@@ -15,11 +15,12 @@ namespace LapTimer
 {
 	public class Main : Script
 	{
-		// You can set your mod information below! Be sure to do this!
+		#region metadata
 		bool firstTime = true;
 		string ModName = "Lap Timer";
 		string Developer = "iLike2Teabag";
 		string Version = "1.0";
+		#endregion
 
 		public Main()
 		{
@@ -50,9 +51,13 @@ namespace LapTimer
 
 		// constants
 		const float checkpointRadius = 8.0f;    // radius in meters of a checkpoint
+		Vector3 checkpointOffset = new Vector3(0.0f, 0.0f, -1.0f);	// modify the standard checkpoint's position by this offset when drawing; cosmetic only!
 
 		// placement mode variables
 		List<SectorCheckpoint> markedSectorCheckpoints = new List<SectorCheckpoint>();                // add to this list when player marks a position
+
+		// race mode variables
+		SectorCheckpoint activeCheckpoint;
 
 
 
@@ -157,23 +162,31 @@ namespace LapTimer
 			// if currently in race mode, try to leave race mode
 			if (raceMode)
 			{
+				// TODO: exitRaceMode()
 				raceMode = false;
 				return false;
 			}
 
-			// first, validate marked checkpoints; stop if checkpoints are invalid
-			if (!validateCheckpoints(markedSectorCheckpoints))
+			// try to enter race mode
+			else
 			{
-				GTA.UI.Screen.ShowSubtitle("~r~Lap Timer: cannot enter race mode.");
-			}
+				// if currently in placement mode, attempt to exit it first.
+				if (placementMode)
+				{
+					togglePlacementMode();
+					// TODO: check that placement mode was exited successfully
+				}
 
-			// if currently in placement mode, attempt to exit and enter race mode.
-			if (placementMode)
-			{
-				togglePlacementMode();
+				// check that the player can enter race mode
+				if (canEnterRaceMode())
+				{
+					enterRaceMode();
+				}
+				else
+					GTA.UI.Screen.ShowSubtitle("~r~Lap Timer: cannot enter Race Mode.");
 			}
-
-			return raceMode;
+			
+			return raceMode;			// return the updated status of the race mode flag
 		}
 
 
@@ -188,16 +201,15 @@ namespace LapTimer
 			SectorCheckpoint newCheckpoint = new SectorCheckpoint();
 
 			// determine current player position; decrease pos.Z by 1 meter such that the checkpoint is on the ground
-			Vector3 playerPos = Game.Player.Character.Position;
-			playerPos.Z -= 1.0f;
-			newCheckpoint.position = playerPos;
+			newCheckpoint.position = Game.Player.Character.Position;
+			newCheckpoint.quarternion = Game.Player.Character.Quaternion;
 
 			// assign SectorCheckpoint number, based on length of markedSectorCheckpoints list.
 			int checkpointNum = markedSectorCheckpoints.Count;
 			newCheckpoint.number = checkpointNum;
 
 			// place marker (blip + checkpoint)
-			newCheckpoint.marker = placeMarker(playerPos, MarkerType.placement, checkpointNum);
+			newCheckpoint.marker = placeMarker(newCheckpoint.position, MarkerType.placement, checkpointNum);
 
 			// print output if verbose
 			if (verbose)
@@ -220,10 +232,15 @@ namespace LapTimer
 		/// </summary>
 		/// <param name="pos">Position as instance of Vector3</param>
 		/// <param name="type">Indicate whether checkpoint is to be used in a race or in placement mode</param>
-		private Marker placeMarker(Vector3 pos, MarkerType type, int number = 0, float radius = checkpointRadius)
+		/// <param name="number">Placement mode only: the number to display on the checkpoint</param>
+		/// <param name="radius">Radius of the checkpoint</param>
+		/// <param name="target">Race mode only: position of the next checkpoint, if applicable. Omit or pass in <c>null</c> if not applicable</param>
+		private Marker placeMarker(Vector3 pos, MarkerType type, int number = 0, float radius = checkpointRadius, Vector3? target = null)
 		{
 			// instantiate empty Marker
 			Marker marker = new Marker();
+
+			// decrease pos and target height (Z) by 1.0 meter, to keep the checkpoint on the ground
 
 			// place a placement mode marker
 			if (type == MarkerType.placement)
@@ -231,7 +248,8 @@ namespace LapTimer
 				// create checkpoint & blip
 				marker.checkpoint = GTA.World.CreateCheckpoint (
 									new GTA.CheckpointCustomIcon(CheckpointCustomIconStyle.Number, Convert.ToByte(number)),
-									pos, pos, radius, Color.FromArgb(255, 255, 66));
+									pos+checkpointOffset, pos+checkpointOffset, 
+									radius, Color.FromArgb(255, 255, 66));
 				marker.blip = GTA.World.CreateBlip(pos);
 			}
 
@@ -289,10 +307,8 @@ namespace LapTimer
 		{
 			for (int i = 0; i < markedSectorCheckpoints.Count; i++)
 			{
-				//if (markedSectorCheckpoints[i].marker.checkpoint != null)
-					markedSectorCheckpoints[i].marker.checkpoint.Delete();          // delete the checkpoint
-				//if (markedSectorCheckpoints[i].marker.blip != null)
-					markedSectorCheckpoints[i].marker.blip.Delete();                // delete the blip
+				markedSectorCheckpoints[i].marker.checkpoint.Delete();          // delete the checkpoint
+				markedSectorCheckpoints[i].marker.blip.Delete();                // delete the blip
 			}
 		}
 
@@ -312,7 +328,59 @@ namespace LapTimer
 
 
 
+		/// <summary>
+		/// Determine whether the player can enter race mode right now. List all reasons why player cannot enter race mode if not.
+		/// </summary>
+		/// <returns><c>true</c> if possible to enter race mode</returns>
+		private bool canEnterRaceMode()
+		{
+			bool ret = true;
 
+			// markedSectorCheckpoints must be valid
+			if (!validateCheckpoints(markedSectorCheckpoints))
+				ret = false;
+
+			// must not be actively on a mission and be able to accept missions
+			if (!Game.Player.CanStartMission)
+			{
+				ret = false;
+				GTA.UI.Notification.Show("~r~Lap Timer: Player cannot start mission. Cannot enter Race Mode.");
+			}
+
+			// must be in control of character
+			if (!Game.Player.CanControlCharacter)
+			{
+				ret = false;
+				GTA.UI.Notification.Show("~r~Lap Timer: Player cannot control character. Cannot enter Race Mode");
+			}
+
+			// must be in a vehicle
+			if (!Game.Player.Character.IsInVehicle())
+			{
+				ret = false;
+				GTA.UI.Notification.Show("~r~Lap Timer: Player must be in a vehicle to enter Race Mode");				
+			}
+
+			return ret;
+		}
+
+
+
+		/// <summary>
+		/// Setup race mode by disabling traffic, clearing weather, and teleporting player to the 1st SectorCheckpoint.
+		/// </summary>
+		private void enterRaceMode(bool verbose = true)
+		{
+			// set the 2nd SectorCheckpoint as active (there must be at least 2 SectorCheckpoints to start race mode); draw the checkpoint
+			activeCheckpoint = markedSectorCheckpoints[1];
+
+			// teleport player to the starting checkpoint; set player orientation
+			Game.Player.Character.CurrentVehicle.Position = activeCheckpoint.position;
+			Game.Player.Character.CurrentVehicle.Quaternion = activeCheckpoint.quarternion;
+
+			if (verbose)
+				GTA.UI.Screen.ShowSubtitle("Lap Timer: Starting race...");
+		}
 
 
 
@@ -341,7 +409,8 @@ namespace LapTimer
 	#region structs
 	struct SectorCheckpoint
 	{
-		public Vector3 position;
+		public Vector3 position;			// Entity.Position
+		public Quaternion quarternion;		// Entity.Quarternion
 		public Marker marker;
 		public int number;
 	}
